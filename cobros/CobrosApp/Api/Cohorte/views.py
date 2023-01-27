01b1e78bd8eb389cab1297402d2b97060c9d8ed4
Carla-troya <troyacarla0@gmail.com>
2023-01-27 02:55:11 -0500

    success en cohorte

--- cobros/CobrosApp/Api/Cohorte/views.py
+++ cobros/CobrosApp/Api/Cohorte/views.py
@@ -11,9 +11,9 @@ class CohorteAV(APIView):
             courses=Cohorte.objects.all()
             serializer=CohorteSerializer(courses,many=True)
             data=serializer.data
-            return Response({'data':data,'succes':True,'message':'Listado de cohortes'},status=status.HTTP_200_OK)
+            return Response({'data':data,'success':True,'message':'Listado de cohortes'},status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'data':data,'succes':False,'message':'Error '+str(e)},status=status.HTTP_404_NOT_FOUND)
+            return Response({'data':data,'success':False,'message':'Error '+str(e)},status=status.HTTP_404_NOT_FOUND)
     def post(self,request):
         data=None
         try:
@@ -21,11 +21,11 @@ class CohorteAV(APIView):
             if serializer.is_valid():
                 serializer.save()
                 data=serializer.data
-                return Response({'data':data,'succes':True,'message':'Cohorte creado exitosamente'},status=status.HTTP_201_CREATED)
+                return Response({'data':data,'success':True,'message':'Cohorte creado exitosamente'},status=status.HTTP_201_CREATED)
             else:
                 return Response({'data':serializer.errors,'success':False,'message':'No se puede crear la cohorte'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({'data':data,'succes':False,'message':'Error '+str(e)},status=status.HTTP_404_NOT_FOUND)
+            return Response({'data':data,'success':False,'message':'Error '+str(e)},status=status.HTTP_404_NOT_FOUND)
 class CohorteDetail(APIView):
     def get(self,request,pk):
         data=None
