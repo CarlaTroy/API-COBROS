44913ca588fd02636ad13ebcf4a775a37425478e
Troya Carla <troyacarla0@gmail.com>
2023-03-04 19:51:54 -0500

    mejornado las migraciones

--- cobros/cobros/settings.py
+++ cobros/cobros/settings.py
@@ -86,7 +86,20 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
+## PRODUCCION 
+## USER :ky4lkexwbsc8_carla_troya
+## PASSWORD : .E5z7tM,Q*Kb
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'ky4lkexwbsc8_bd_cobros',
+        'USER' : 'ky4lkexwbsc8_carla_troya',
+        'PASSWORD' : 'ix-nl*A-#9=@',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+    }
+} """
 
 
 # Password validation
